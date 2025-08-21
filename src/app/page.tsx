ec5345e6d7b9009cf5cdece1941422ba67b1ec0d
kenesuexe <kenesuexe@gmail.com>
2025-08-21 14:38:07 +0800

    Initialized website

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-900 text-white">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold tracking-tight mb-4">
+          Hello, world!
+        </h1>
+        <p className="text-2xl font-light text-gray-400">
+          Stay tuned. - kenesuexe
+        </p>
+      </div>
+    </main>
+  );
+}
